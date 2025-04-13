ab93b7009cae8f526bee337b748959f33e9f7665
EunhaKyeong <kyeongeh424@gmail.com>
2025-04-14 00:00:58 +0900

    ✨ 피드 공통 컴포넌트 추가 > 피드 이미지

---
+++ src/components/Feed/FeedImage.tsx
@@ -0,0 +1,89 @@
+import { Image, StyleSheet, View } from "react-native";
+import { Button } from "../Button";
+import { Svg } from "../Svg";
+import PagerView from "react-native-pager-view";
+import { colors } from "@/theme/colors";
+import { useState } from "react";
+
+interface FeedImageProps {
+    images: string[];
+}
+
+const FeedImageItem = (
+    image: string,
+    index: number,
+) => (
+    <View key={index}>
+        <Image
+            style={feedImageStyles.feedImage}
+            source={{ uri: image }}
+        />
+        <Button style={feedImageStyles.moreButton}>
+            <Svg svgName="FeedDumbbell" />
+        </Button>
+    </View>
+);
+
+export const FeedImage = (props: FeedImageProps) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+    return (
+        <View>
+            <PagerView
+                style={feedImageStyles.rootView}
+                initialPage={currentImageIndex}
+                onPageScroll={(event) => { setCurrentImageIndex(event.nativeEvent.position) }}>
+                {
+                    props.images.map((image, index) => (
+                        FeedImageItem(image, index)
+                    ))
+                }
+            </PagerView>
+            {
+                props.images.length === 1 ? null : <View style={feedImageStyles.indicator}>
+                    {
+                        props.images.map((_, index) => (
+                            currentImageIndex === index ? <View key={index} style={feedImageStyles.currentIndicatorItem} /> : <View key={index} style={feedImageStyles.otherIndicatorItem} />
+                        ))
+                    }
+                </View>
+            }
+        </View>
+    );
+};
+
+const feedImageStyles = StyleSheet.create({
+    rootView: {
+        width: "100%",
+        aspectRatio: 1 / 0.95,
+        alignItems: "stretch",
+    },
+    feedImage: {
+        position: "relative",
+        flex: 1,
+    },
+    moreButton: {
+        position: "absolute",
+        bottom: 10,
+        left: 10,
+    },
+    indicator: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "center",
+        marginVertical: 8,
+        gap: 5,
+    },
+    currentIndicatorItem: {
+        width: 5,
+        height: 5,
+        borderRadius: 2.5,
+        backgroundColor: colors.PRIMARY,
+    },
+    otherIndicatorItem: {
+        width: 5,
+        height: 5,
+        borderRadius: 2.5,
+        backgroundColor: "#D8D8D8",
+    },
+});

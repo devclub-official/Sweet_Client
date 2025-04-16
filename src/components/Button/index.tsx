bcb809577c12ffb7eaba0ae20e9104757991c97c
KyeongS <59828677+leeks9653@users.noreply.github.com>
2025-04-16 22:19:34 +0900

    ✨ Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,62 @@
-import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import {colors} from '@/theme/colors';
+import {
+  StyleProp,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ViewStyle,
+} from 'react-native';
 
-interface Props extends TouchableOpacityProps {}
-export const Button = (props: Props) => {
-  return <TouchableOpacity {...props} />;
+interface Props extends TouchableOpacityProps {
+  type?: 'primary' | 'secondary' | 'black';
+  size?: 'large' | 'small';
+  fill?: boolean;
+}
+export const Button = ({
+  type = 'primary',
+  size = 'large',
+  fill = true,
+  ...rest
+}: Props) => {
+  const fillStyle = '';
+  const sizeStyle = '';
+  const typeStyle = '';
+
+  const createButtonStyle = (): StyleProp<ViewStyle> => {
+    const style: StyleProp<ViewStyle> = {};
+    if (fill) {
+      switch (type) {
+        case 'primary':
+          style.backgroundColor = colors.PRIMARY;
+          break;
+        case 'secondary':
+          style.backgroundColor = colors.SECONDARY;
+          break;
+        default:
+          style.backgroundColor = colors.BLACK;
+      }
+    } else {
+      style.borderWidth = 1;
+      switch (type) {
+        case 'primary':
+          style.borderColor = colors.PRIMARY;
+          break;
+        case 'secondary':
+          style.borderColor = colors.SECONDARY;
+          break;
+        default:
+          style.borderColor = colors.BLACK;
+      }
+    }
+
+    return {};
+  };
+
+  return <TouchableOpacity {...rest} style={[styles.buttonDefaultStyle]} />;
 };
+
+const styles = StyleSheet.create({
+  buttonDefaultStyle: {
+    borderRadius: 10,
+  },
+});

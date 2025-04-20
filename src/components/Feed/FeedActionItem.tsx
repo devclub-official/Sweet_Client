132ac970b29b845f4156dbfde73495a958017483
EunhaKyeong <kyeongeh424@gmail.com>
2025-04-20 23:09:39 +0900

    ✨ 피드 컴포넌트 > 좋아요/댓글/공유하기 버튼 영역 퍼블리싱

---
+++ src/components/Feed/FeedActionItem.tsx
@@ -0,0 +1,33 @@
+//피드 컴포넌트 중 좋아요/댓글/공유하기 버튼 영역입니다.
+
+import { StyleSheet, View } from "react-native";
+import { Button } from "../Button";
+import { Typo } from "../Typo";
+import { Svg } from "../Svg";
+import { SvgNames } from "@/types/svg";
+
+interface FeedActionItemProps {
+    svgName: SvgNames;
+    count?: number;
+    onPress: () => void;
+}
+
+export const FeedActionItem = ({ svgName, count, onPress }: FeedActionItemProps) => (
+    <View style={styles.rootContainer}>
+        <Button onPress={onPress}>
+            <Svg svgName={svgName} />
+        </Button>
+        <Typo style={styles.feedActionsTypo}>{count}</Typo>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
+    },
+    feedActionsTypo: {
+        color: '#CDCDCD',
+    },
+});

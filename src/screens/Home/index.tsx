2628807fbfe82da3ee7d1bd76d17d45ea1172cc8
KyeongS <59828677+leeks9653@users.noreply.github.com>
2025-04-26 20:05:49 +0900

    ✨ Apple login

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,11 @@
-import {testF} from '@/apis/auth';
-import {api} from '@/apis/common';
+import {View} from 'react-native';
 import {Button} from '@/components/Button';
 import {Typo} from '@/components/Typo';
 import {useSweetNavigation} from '@/hooks/useNavigation';
 import {useAuthStore} from '@/stores/useAuthStore';
 import {RootStackScreenList, RouteParams} from '@/types/navigation';
 import {useNavigation, useRoute} from '@react-navigation/native';
-import {View} from 'react-native';
+import {appleAuth} from '@invertase/react-native-apple-authentication';
 
 export const Home = () => {
   const navigation = useNavigation();
@@ -14,6 +13,22 @@ export const Home = () => {
   const {push} = useSweetNavigation();
 
   const {setAccessToken, accessToken} = useAuthStore();
+
+  const onAppleButtonPress = async () => {
+    try {
+      const appleAuthRequestResponse = await appleAuth.performRequest({
+        requestedOperation: appleAuth.Operation.LOGIN,
+        requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+      });
+
+      console.log('애플 로그인 성공!');
+      console.log('appleAuthRequestResponse ==>', appleAuthRequestResponse);
+
+      // identityToken을 서버에 보내서 본인 인증하면 완벽하게 안전!
+    } catch (error) {
+      console.error('애플 로그인 실패', error);
+    }
+  };
 
   return (
     <View>
@@ -39,6 +54,13 @@ export const Home = () => {
           push(RootStackScreenList.Onboard);
         }}>
         스택 화면 이동
+      </Button>
+      <Button
+        size="small"
+        onPress={() => {
+          onAppleButtonPress();
+        }}>
+        소셜 로그인 애플
       </Button>
 
       <Typo>{accessToken}</Typo>

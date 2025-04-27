d5cdc82cc32813dbabbbba710ba317a90ad2e82b
KyeongS <59828677+leeks9653@users.noreply.github.com>
2025-04-27 23:54:21 +0900

    ✨ 로그인 화면 퍼블리싱

--- src/screens/Login/components/SocialLoginButton.tsx
+++ src/screens/Login/components/SocialLoginButton.tsx
@@ -1,10 +1,70 @@
+import {Svg} from '@/components/Svg';
 import {Typo} from '@/components/Typo';
-import {TouchableOpacity} from 'react-native';
+import {colors} from '@/theme/colors';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
 
-export const SocialLoginButton = () => {
-  return (
-    <TouchableOpacity>
-      <Typo font="BodyLargeR">안녕</Typo>
-    </TouchableOpacity>
-  );
+interface Props extends TouchableOpacityProps {
+  type: 'apple' | 'naver' | 'kakao';
+}
+
+export const SocialLoginButton = ({type}: Props) => {
+  const renderButton = () => {
+    switch (type) {
+      case 'apple':
+        return (
+          <View style={[styles.button, styles.kakao]}>
+            <Svg svgName="AppleLogin" />
+            <Typo font="BodyLargeR" style={styles.font}>
+              Apple로 계속하기
+            </Typo>
+          </View>
+        );
+      case 'kakao':
+        return (
+          <View style={[styles.button, styles.naver]}>
+            <Svg svgName="KakaoLogin" />
+            <Typo font="BodyLargeR" style={styles.font}>
+              카카오톡으로 계속하기
+            </Typo>
+          </View>
+        );
+      case 'naver':
+        return (
+          <View style={[styles.button, styles.apple]}>
+            <Svg svgName="NaverLogin" />
+            <Typo font="BodyLargeR" color="B_BASE_PRI" style={styles.font}>
+              네이버로로 계속하기
+            </Typo>
+          </View>
+        );
+    }
+  };
+  return <TouchableOpacity>{renderButton()}</TouchableOpacity>;
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    borderRadius: 10,
+    padding: 16,
+    gap: 10,
+  },
+  font: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  kakao: {
+    backgroundColor: 'yellow',
+  },
+  naver: {
+    backgroundColor: 'green',
+  },
+  apple: {
+    backgroundColor: colors.B_50,
+  },
+});
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,11 +1,14 @@
-import {Typo} from '@/components/Typo';
 import appleAuth from '@invertase/react-native-apple-authentication';
 import {StatusBar, StyleSheet, View} from 'react-native';
 import {useCallback} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
 import {Svg} from '@/components/Svg';
+import LinearGradient from 'react-native-linear-gradient';
+import {SocialLoginButton} from './components/SocialLoginButton';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const Login = () => {
+  const insets = useSafeAreaInsets();
   const onAppleButtonPress = async () => {
     try {
       const appleAuthRequestResponse = await appleAuth.performRequest({
@@ -29,15 +32,61 @@ export const Login = () => {
   );
 
   return (
-    <View>
-      <View style={styles.contentWrapper}>
-        <Svg svgName="AppLogo" />
-        <Typo>login</Typo>
-      </View>
+    <View style={styles.wrapper}>
+      <LinearGradient
+        colors={['#66FBEF', '#66FBA5']}
+        style={styles.gradient}
+        start={{x: 0.0, y: 0.0}}
+        end={{x: 1.0, y: 1.0}}>
+        <View
+          style={[styles.contentWrapper, {paddingBottom: 40 + insets.bottom}]}>
+          <View style={styles.logoWrapper}>
+            <Svg svgName="AppLogo" />
+          </View>
+          <View style={styles.buttonWrapper}>
+            <SocialLoginButton
+              onPress={() => {
+                console.log('good ==> ');
+              }}
+              type="kakao"
+            />
+            <SocialLoginButton
+              onPress={() => {
+                console.log('good ==> ');
+              }}
+              type="naver"
+            />
+            <SocialLoginButton
+              onPress={() => {
+                console.log('good ==> ');
+              }}
+              type="apple"
+            />
+          </View>
+        </View>
+      </LinearGradient>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentWrapper: {},
+  wrapper: {
+    flex: 1,
+  },
+  contentWrapper: {
+    flex: 1,
+    paddingBottom: 40,
+  },
+  gradient: {
+    flex: 1,
+  },
+  logoWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    gap: 16,
+    paddingHorizontal: 20,
+  },
 });

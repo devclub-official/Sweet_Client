b5324b0493f38d38c889485d15b48007cfdb2789
EunhaKyeong <kyeongeh424@gmail.com>
2025-05-04 00:37:25 +0900

    ✨ 마이페이지 화면에서 설정화면으로 이동하는 로직 추가

--- src/screens/MyPage/components/HeaderRight.tsx
+++ src/screens/MyPage/components/HeaderRight.tsx
@@ -1,13 +1,23 @@
 import { Menu } from "@/assets/svgs/Menu";
 import { NewAlarm } from "@/assets/svgs/NewAlarm";
-import { StyleSheet, View } from "react-native";
+import { useSweetNavigation } from "@/hooks/useNavigation";
+import { RootStackScreenList } from "@/types/navigation";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
-export const HeaderRight = () => (
-    <View style={styles.rootContainer}>
-        <NewAlarm />
-        <Menu />
-    </View>
-);
+export const HeaderRight = () => {
+    const navigation = useSweetNavigation();
+
+    return (
+        <View style={styles.rootContainer}>
+            <NewAlarm />
+            <TouchableOpacity onPress={() => {
+                navigation.push(RootStackScreenList.Setting);
+            }}>
+                <Menu />
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     rootContainer: {

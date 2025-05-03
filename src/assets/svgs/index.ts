41d3a000075f7c1e26e353fe9dc9f216b3f67940
EunhaKyeong <kyeongeh424@gmail.com>
2025-05-03 23:34:32 +0900

    ✨ 프로필 컴포넌트 퍼블리싱

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,9 +1,13 @@
+import { Calendar } from './Calendar';
 import {ChevronLeft} from './ChevronLeft';
 import {ChevronRight} from './ChevronRight';
 import {Dumbbell} from './Dumbbell';
 import {Eye} from './Eye';
+import { Fire } from './Fire';
 import {Home} from './Home';
 import {Location} from './Location';
+import { Menu } from './Menu';
+import { NewAlarm } from './NewAlarm';
 import {Profile} from './Profile';
 
 export const svgMap = {
@@ -14,4 +18,8 @@ export const svgMap = {
   ChevronRight,
   Location,
   Eye,
+  Fire,
+  Calendar,
+  NewAlarm,
+  Menu,
 };

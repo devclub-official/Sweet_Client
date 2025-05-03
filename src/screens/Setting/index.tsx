8713c5c7ff403ebe970a5b750be03ec9150a3fde
EunhaKyeong <kyeongeh424@gmail.com>
2025-05-04 00:37:09 +0900

    ✨ 설정 및 개인정보 관리 화면 퍼블리싱

---
+++ src/screens/Setting/index.tsx
@@ -0,0 +1,59 @@
+import { SafeAreaScreenWrapper } from "@/components/SafeAreaScreenWrapper"
+import { AppStatusBar } from "@/components/StatusBar"
+import { colors } from "@/theme/colors"
+import { SettingLayout } from "./components/SettingLayout";
+import { StyleSheet, View } from "react-native";
+import { SettingItem } from "./components/SettingItem";
+import { Divider } from "@/components/Divider";
+import { Typo } from "@/components/Typo";
+import { strings as settingString } from "./constants/strings";
+import { strings } from "@/constants/strings";
+
+export const Setting = () => {
+    return (
+        <SafeAreaScreenWrapper>
+            <AppStatusBar backgroundColor={colors.B_BASE_PRI} />
+            <View style={styles.rootContainer}>
+                <SettingLayout>
+                    <SettingItem
+                        title={settingString.TERMS_OF_SERVICE}
+                        onPressNext={() => { }}
+                    />
+                    <Divider style={styles.divider} />
+                    <View style={styles.versionContainer}>
+                        <Typo color="B_50" font="SubSmallM">{settingString.VERSION}</Typo>
+                        <Typo color="B_50" font="BodyMediumR">0.0.1</Typo>
+                    </View>
+                </SettingLayout>
+                <SettingLayout>
+                    <SettingItem
+                        title={strings.WITHDRAW}
+                        onPressNext={() => { }}
+                    />
+                </SettingLayout>
+                <SettingLayout>
+                    <SettingItem
+                        title={strings.LOGOUT}
+                        onPressNext={() => { }}
+                    />
+                </SettingLayout>
+            </View>
+        </SafeAreaScreenWrapper>
+    );
+};
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        gap: 10,
+    },
+    divider: {
+        marginVertical: 10,
+    },
+    versionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+});

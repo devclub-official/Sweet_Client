ccc2b0b2567c65de1d0667331d6160c9bd53f73a
EunhaKyeong <kyeongeh424@gmail.com>
2025-06-15 16:54:13 +0900

    ⚡️ 프로필 편집 api 에 프로필 사진 데이터 추가

--- src/screens/EditProfile/hooks/useEditProfile.ts
+++ src/screens/EditProfile/hooks/useEditProfile.ts
@@ -4,32 +4,37 @@ import { patchUserAPI } from "@/apis/user";
 import { EditProfile } from "@/models/domain/Profile/EditProfile";
 import { useCallback, useEffect, useState } from "react";
 import Config from "react-native-config";
+import { Asset } from "react-native-image-picker";
 
 export const useEditProfile = () => {
     const [profile, setProfile] = useState<EditProfile>({
         nickname: '',
-        profileImage: 'https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        profileImage: '',
         introduce: '',
     });
 
+    const [newImage, setNewImage] = useState<Asset>();
     const [newNickname, setNewNickname] = useState<string>('');
     const [newIntroduce, setNewIntroduce] = useState<string>('');
 
     const updateProfile = useCallback(() => {
-        patchUserAPI({
-            nickname: newNickname,
-            bio: newIntroduce,
-        }).then(res => {
-            setProfile(prev => ({
-                ...prev,
+        patchUserAPI(
+            newImage,
+            {
                 nickname: newNickname,
-                introduce: newIntroduce,
-            }));
-        }).catch(err => {
-            if (err instanceof SweetError) {
-                console.log(err.errorMessage);
+                bio: newIntroduce,
             }
-        });
+        ).then(res => {
+                setProfile({
+                    profileImage: `${Config.MAIN_API_ORIGIN}${res.data.profileImage}`,
+                    nickname: newNickname,
+                    introduce: newIntroduce,
+                });
+            }).catch(err => {
+                if (err instanceof SweetError) {
+                    console.log(err.errorMessage);
+                }
+            });
     }, [newNickname, newIntroduce]);
 
     useEffect(() => {
@@ -50,8 +55,10 @@ export const useEditProfile = () => {
 
     return {
         profile,
+        newImage,
         newNickname,
         newIntroduce,
+        setNewImage,
         setNewNickname,
         setNewIntroduce,
         updateProfile,

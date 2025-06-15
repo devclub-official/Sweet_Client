69811a3dad5e9afefba725e13702a8c19db54257
EunhaKyeong <kyeongeh424@gmail.com>
2025-06-15 17:05:50 +0900

    ✨ 유저 프로필 조회 api 연동

--- src/components/Profile/hooks/useProfile.tsx
+++ src/components/Profile/hooks/useProfile.tsx
@@ -1,10 +1,10 @@
 import { useCallback, useEffect, useState } from "react"
 import { ProfileInfo } from "@/models/domain/Profile/ProfileInfo";
-import { fetchMyProfileAPI } from "@/apis/profile";
-import { getMyProfileDtoToDomain } from "@/models/mapper/Profile";
+import { fetchMyProfileAPI, fetchUserProfileAPI } from "@/apis/profile";
+import { getMyProfileDtoToDomain, getUserProfileDtoToDomain } from "@/models/mapper/Profile";
 import { SweetError } from "@/apis/error";
 
-export const useProfile = (userId: number, isMyPage: boolean) => {
+export const useProfile = (userId?: number, isMyPage: boolean) => {
     const [profileInfo, setProfileInfo] = useState<ProfileInfo>({
         id: "-1",
         type: "SELF",
@@ -32,9 +32,25 @@ export const useProfile = (userId: number, isMyPage: boolean) => {
             });
     }, []);
 
+    const getUserProfileInfo = useCallback(() => {
+        if (userId) {
+            fetchUserProfileAPI(userId)
+                .then(res => {
+                    setProfileInfo(getUserProfileDtoToDomain(res));
+                })
+                .catch(err => {
+                    if (err instanceof SweetError) {
+                        console.log(err.errorMessage);
+                    }
+                });
+        }
+    }, [userId]);
+
     useEffect(() => {
         if (isMyPage) {
             getMyProfileInfo();
+        } else {
+            getUserProfileInfo();
         }
     }, [isMyPage]);
 
